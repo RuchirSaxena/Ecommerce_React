7033437559e11f95b0a5be58e019a2cc4541a22b
Ruchir Saxena <ruchirsaxena@gmail.com>
2020-09-05 22:18:11 +0530

    Intial Project Setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+//import './fonts/FiraSans-Regulat.ttf';
+import './fonts/FiraSans-Regular.ttf';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
